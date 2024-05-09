bf062539fe36fe2bc1654757903e3c025daa3335
Sarvar <sabdumutolibov@gmail.com>
2024-05-09 21:06:14 +0500

    lesson19

---
+++ main.py
@@ -0,0 +1,46 @@
+#Task1
+# def num(lst):
+#     if lst % 2 == 0:
+#         return  lst // 2, lst // 2
+#     elif lst % 2 == 1:
+#         return (lst + 1) // 2, (lst - 1) // 2
+#
+#     return lst
+#
+# print(num(4))
+# print(num(10))
+# print(num(11))
+# print(num(-9))
+
+#Task2
+# def nums(lst,s):
+#     if s == "Asc":
+#         return sorted(lst)
+#     elif s == "Des":
+#         return sorted(lst,reverse=True)
+#     elif s == "None":
+#         return lst
+#
+#
+# print(nums([4,3,2,1], "Asc"))
+# print(nums([7,8,11,66],"Des"))
+# print(nums([1,2,3,4],"None"))
+
+
+#task3
+# def numbers(lst):
+#     a = sum(lst)
+#     if a > 1:
+#         return f"{a} ohms"
+#     elif a == 1:
+#         return f"{a} ohm"
+#
+#
+# print(numbers([1,5,6,3]))
+# print(numbers([16,3.5,6]))
+# print(numbers([0.5,0.5]))
+
+
+
+
+
